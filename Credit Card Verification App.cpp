4562fed0d4fe7b366bdfabd4543f0e59ac407fa8
Ashfinn <obidur.shawal@gmail.com>
2023-05-10 23:17:18 +0600

    done with the app... : D

--- Credit Card Verification App.cpp
+++ Credit Card Verification App.cpp
@@ -3,24 +3,25 @@
 #include <vector>
 
 std::string validateCreditCardNumber(std::string input) {
-    std::vector<int> creditCardInt;
+    std::vector<int> creditCardNumber;
     
     for (int i{ 0 };i < input.length();i++) {
-        creditCardInt.push_back(input[i] - '0');
+        creditCardNumber.push_back(input[i] - '0');
     }
     
-    for (int i{ input.length() - 2 }; i >= 0; i -= 2) {
-        int temp{ creditCardInt[i] };
+    for (int i{static_cast<int>( input.length() - 2) }; i >= 0; i -= 2) {
+        int temp{ creditCardNumber[i] };
         temp *= 2;
         if (temp > 9) {
             temp = temp % 10 + 1;
         }
-        creditCardInt[i] = temp;
+        creditCardNumber[i] = temp;
     }
 
     int sum{ 0 };
-    for (int i{ 0 }; i < creditCardInt.size(); i++) {
-        sum += creditCardInt[i];
+    size_t size = creditCardNumber.size();
+    for (size_t i{ 0 }; i < size; i++) {
+        sum += creditCardNumber[i];
     }
     if (sum % 10 == 0) {
         return "The Credit Card Number is Valid";
@@ -34,7 +35,7 @@ int main()
 {
     std::cout << "Welcome to my Credit Card Verification App\n";
     std::cout << "Enter Your Credit Card Number: \n";
-    std::string userNumber{};
+    std::string userNumber;
     std::cin >> userNumber;
     
     std::cout << validateCreditCardNumber(userNumber);
